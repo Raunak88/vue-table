1d6cc9100d9f666c1cb1fdba4f00758c6fbfd239
raunak88 <raunak.kumar88@gmail.com>
2020-08-22 11:12:46 +0530

    added table component

---
+++ src/components/Table.vue
@@ -0,0 +1,15 @@
+<template>
+  <table></table>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
