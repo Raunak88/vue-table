416a475424842640079883bded9af526094227c5
raunak88 <raunak.kumar88@gmail.com>
2020-08-22 11:54:48 +0530

    Rendering data on the table

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Table>
+   <Table v-if="tableData" :childData="tableData" :configData="config"/>
   </div>
 </template>
 
 <script>
-import Table from './components/Table.vue'
-
+import Table from './components/Table';
 export default {
   name: 'App',
   components: {
     Table
+  },  
+  data:()=>({
+    tableData:undefined,
+    config:[
+      {
+        key:'name',
+        title:'Name'
+      },
+       {
+        key:'email',
+        title:'Email'
+      },
+       {
+        key:'phone',
+        title:'Phone'
+      },
+       {
+        key:'company',
+        title:'Company'
+      },
+
+       {
+        key:'createdAt',
+        title:'Creation Date',
+        type:'date'
+      }
+    ]  
+
+    
+  }),
+  mounted(){
+    this.$axios.get('https://5f40a2dea5e9db0016301f4a.mockapi.io/assignment/users')
+    .then((res)=>{
+      this.tableData=res.data;
+    })
+    .catch((err)=>console.log(err))
   }
 }
 </script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,11 +1,25 @@
 <template>
-  <table></table>
+ <table>
+   <thead>
+     <tr>
+       <th v-for="(col,index) in configData" :key="index">{{col.title}}</th>
+     </tr>
+   </thead>
+   <tbody>
+     <tr v-for="(person,i) in childData" :key="i">
+       <td v-for="(col,index) in configData" :key="index">
+         <span v-if="!col.type">{{person[col.key]}}</span>
+         <span v-if="col.type==='date'">{{new Date(person[col.key]).toDateString()}}</span>
+       </td>
+     </tr>
+   </tbody>
+ </table>
 </template>
 
 <script>
-export default {
-  name: 'Table',
-  
+export default{
+  name:'Table',
+  props:['childData','configData']
 }
 </script>
 
